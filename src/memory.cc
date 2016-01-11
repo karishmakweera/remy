d09efcf7b9a11f764bf9271894cfd6b1842ab6e3
Kexin Rong <kexinrongg@gmail.com>
2016-01-11 13:53:26 -0800

    Train only with signal rec_ewma

--- src/memory.cc
+++ src/memory.cc
@@ -8,7 +8,6 @@ using namespace std;
 
 static const double alpha = 1.0 / 8.0;
 
-static const double slow_alpha = 1.0 / 256.0;
 
 void Memory::packets_received( const vector< Packet > & packets, const unsigned int flow_id )
 {
@@ -23,16 +22,11 @@ void Memory::packets_received( const vector< Packet > & packets, const unsigned 
       _last_tick_received = x.tick_received;
       _min_rtt = rtt;
     } else {
-      _rec_send_ewma = (1 - alpha) * _rec_send_ewma + alpha * (x.tick_sent - _last_tick_sent);
       _rec_rec_ewma = (1 - alpha) * _rec_rec_ewma + alpha * (x.tick_received - _last_tick_received);
-      _slow_rec_rec_ewma = (1 - slow_alpha) * _slow_rec_rec_ewma + slow_alpha * (x.tick_received - _last_tick_received);
-
       _last_tick_sent = x.tick_sent;
       _last_tick_received = x.tick_received;
 
       _min_rtt = min( _min_rtt, rtt );
-      _rtt_ratio = double( rtt ) / double( _min_rtt );
-      assert( _rtt_ratio >= 1.0 );
     }
   }
 }
@@ -40,7 +34,7 @@ void Memory::packets_received( const vector< Packet > & packets, const unsigned 
 string Memory::str( void ) const
 {
   char tmp[ 256 ];
-  snprintf( tmp, 256, "sewma=%f, rewma=%f, rttr=%f, slowrewma=%f", _rec_send_ewma, _rec_rec_ewma, _rtt_ratio, _slow_rec_rec_ewma );
+  snprintf( tmp, 256, "rewma=%f", _rec_rec_ewma);
   return tmp;
 }
 
@@ -53,10 +47,7 @@ const Memory & MAX_MEMORY( void )
 RemyBuffers::Memory Memory::DNA( void ) const
 {
   RemyBuffers::Memory ret;
-  ret.set_rec_send_ewma( _rec_send_ewma );
   ret.set_rec_rec_ewma( _rec_rec_ewma );
-  ret.set_rtt_ratio( _rtt_ratio );
-  ret.set_slow_rec_rec_ewma( _slow_rec_rec_ewma );
   return ret;
 }
 
@@ -65,10 +56,7 @@ RemyBuffers::Memory Memory::DNA( void ) const
   ( (protobuf).has_ ## field() ? (protobuf).field() : (limit) ? 0 : 163840 )
 
 Memory::Memory( const bool is_lower_limit, const RemyBuffers::Memory & dna )
-  : _rec_send_ewma( get_val_or_default( dna, rec_send_ewma, is_lower_limit ) ),
-    _rec_rec_ewma( get_val_or_default( dna, rec_rec_ewma, is_lower_limit ) ),
-    _rtt_ratio( get_val_or_default( dna, rtt_ratio, is_lower_limit ) ),
-    _slow_rec_rec_ewma( get_val_or_default( dna, slow_rec_rec_ewma, is_lower_limit ) ),
+  : _rec_rec_ewma( get_val_or_default( dna, rec_rec_ewma, is_lower_limit ) ),
     _last_tick_sent( 0 ),
     _last_tick_received( 0 ),
     _min_rtt( 0 )
@@ -78,10 +66,6 @@ Memory::Memory( const bool is_lower_limit, const RemyBuffers::Memory & dna )
 size_t hash_value( const Memory & mem )
 {
   size_t seed = 0;
-  boost::hash_combine( seed, mem._rec_send_ewma );
   boost::hash_combine( seed, mem._rec_rec_ewma );
-  boost::hash_combine( seed, mem._rtt_ratio );
-  boost::hash_combine( seed, mem._slow_rec_rec_ewma );
-
   return seed;
 }
