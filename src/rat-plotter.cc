55abb31d2f90fbe92cc6d7458c409e13074e71c1
Kexin Rong <kexinrongg@gmail.com>
2016-01-27 11:46:19 -0800

    Seperate plotting script from rat-runner

--- src/rat-plotter.cc
+++ src/rat-plotter.cc
@@ -1,27 +1,40 @@
 #include <cstdio>
 #include <vector>
 #include <string>
-#include <limits>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <fstream>
+#include <iostream>
 
 #include "evaluator.hh"
 #include "configrange.hh"
 using namespace std;
+
+double parse_outcome(Evaluator::Outcome outcome) {
+  double norm_score = 0;
+  for ( auto &run : outcome.throughputs_delays ) {
+    printf( "===\nconfig: %s\n", run.first.str().c_str() );
+    for ( auto &x : run.second ) {
+      printf( "sender: [tp=%f, del=%f]\n", x.first / run.first.link_ppt, x.second / run.first.delay );
+      norm_score += log2( x.first / run.first.link_ppt ) - log2( x.second / run.first.delay );
+    }
+  }  
+  return norm_score;
+}
 
 int main( int argc, char *argv[] )
 {
   WhiskerTree whiskers;
   unsigned int num_senders = 2;
   double link_ppt = 1.0;
-  double delay = 100.0;
-  double mean_on_duration = 5000.0;
-  double mean_off_duration = 5000.0;
-  double buffer_size = numeric_limits<unsigned int>::max();
-  ofstream linkspeed_out;
-  bool gen_linkspeed = false;
+  double delay = 150.0;
+  double mean_on_duration = 1000.0;
+  double mean_off_duration = 1000.0;
+  int num_points = 100; /* Granularity of the generated plot */
+  ofstream plot;
+  bool linkspeed = false;
 
   for ( int i = 1; i < argc; i++ ) {
     string arg( argv[ i ] );
@@ -67,37 +80,39 @@ int main( int argc, char *argv[] )
     } else if ( arg.substr( 0, 4 ) == "off=" ) {
       mean_off_duration = atof( arg.substr( 4 ).c_str() );
       fprintf( stderr, "Setting mean_off_duration to %f ms\n", mean_off_duration );
-    } else if ( arg.substr( 0, 4 ) == "buf=" ) {
-      if (arg.substr( 4 ) == "inf") {
-        buffer_size = numeric_limits<unsigned int>::max();
-      } else {
-        buffer_size = atoi( arg.substr( 4 ).c_str() );
-      }
+    } else if ( arg.substr( 0, 6 ) == "granu=") {
+      num_points = atof( arg.substr( 6 ).c_str() );
+      fprintf( stderr, "Setting granularity to %d points\n", num_points );
+    } else if ( arg.substr( 0, 10 ) == "linkspeed=") {
+      string filename( arg.substr( 10 ) );
+      plot.open(filename);
+      linkspeed = true;
+      fprintf( stderr, "Outputting to %s\n", filename.c_str() );
     }
    }
 
   ConfigRange configuration_range;
   configuration_range.link_ppt = Range( link_ppt,link_ppt, 0 );
   configuration_range.rtt = Range( delay, delay, 0 ); /* ms */
-  configuration_range.num_senders = Range( num_senders, num_senders, 0 );
-  configuration_range.mean_on_duration = Range( mean_on_duration, mean_on_duration, 0 );
-  configuration_range.mean_off_duration = Range( mean_off_duration, mean_off_duration, 0 );
-  configuration_range.buffer_size = Range( buffer_size, buffer_size, 0 );
-  Evaluator eval( configuration_range );
-  auto outcome = eval.score( whiskers, false, 10 );
-  printf( "score = %f\n", outcome.score );
-  double norm_score = 0;
-
-  for ( auto &run : outcome.throughputs_delays ) {
-    printf( "===\nconfig: %s\n", run.first.str().c_str() );
-    for ( auto &x : run.second ) {
-      printf( "sender: [tp=%f, del=%f]\n", x.first / run.first.link_ppt, x.second / run.first.delay );
-      norm_score += log2( x.first / run.first.link_ppt ) - log2( x.second / run.first.delay );
+  configuration_range.num_senders = Range(num_senders, num_senders, 0 );
+  configuration_range.mean_on_duration = Range(mean_on_duration, mean_on_duration, 0);
+  configuration_range.mean_off_duration = Range(mean_off_duration, mean_off_duration, 0);
+  if (linkspeed) {
+    /* Link speed range we are evaluating is 1 to 1000 Mbps. */
+    link_ppt = 0.1;
+    double step = pow(10, 3.0 / (num_points - 1));
+    for (int i = 0; i < num_points; i ++) {
+      configuration_range.link_ppt = Range( link_ppt,link_ppt, 0 );
+      Evaluator eval( configuration_range );
+      auto outcome = eval.score( whiskers, false, 10 );
+      double norm_score = parse_outcome(outcome);
+      plot << to_string(link_ppt * 10) + " ";
+      plot << to_string(norm_score / 2) + "\n";
+      link_ppt *= step;
     }
-  }  
+    plot.close();
+  }
   
-  printf( "normalized_score = %f\n", norm_score );
-  printf( "Whiskers: %s\n", outcome.used_whiskers.str().c_str() );
 
   return 0;
 }
